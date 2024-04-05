61e7dd184ac3e896985c56a1c7cac9e2e260f6f3
SHDarkStar <158495755+SHDarkStar@users.noreply.github.com>
2024-04-05 15:48:25 +0330

    Update Mainbar.tsx

--- src/pages/Mainbar.tsx
+++ src/pages/Mainbar.tsx
@@ -1,21 +1,18 @@
-import "./mainbarStyle.css";
-export default function Mainbar() {
+import "./styles.css";
+export default function MainBar() {
 	return (
-	<section
-		className="mainbar">
+	<div className="mainBar">
 		<div className="main">
-		<div>
-			<h4 style={{"fontSize":"1.25rem","lineHeight":"1.75rem"}}>
-				Hi and welcome to
+			<h4 style={{"fontSize":"20px"}}>
+			Hi and welcome to
 			</h4>
-			<h2 style={{"fontSize":"3rem","lineHeight":1}}>
-			My Portfolio Website!
+			<h2 style={{"fontSize":"60px" }}>
+			My Portfolio Website <span className="animatedText">!</span>
 			</h2>
+			<h5 style={{"fontSize":"20px","color": "rgb(227,224,222)"}}>
+			I am a Python programmer, currently learning React.
+			</h5>
 		</div>
-		<h5 style={{"fontSize":"1.25rem","lineHeight":"1.75rem"}}>
-			I am a python programmer, currently learning React.
-		</h5>
-		</div>
-	</section>
+	</div>
 	);
 }
