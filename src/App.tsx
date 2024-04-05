cd3bc88f4842e09185b5a899e98ad221697280d1
SHDarkStar <158495755+SHDarkStar@users.noreply.github.com>
2024-04-05 15:51:35 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
+//تمام صفحات ساخته شده در این بخش به همدیگر متصل میشوند و به ترتیب 4 صفحه و navigation را وارد میکنیم
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from "./pages/Navbar";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
-import Mainbar from "./pages/Mainbar";
+import MainBar from "./pages/Mainbar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<div>
 				<Navbar />
-				<div style={{ border: '1px solid blue', padding: 10 }}>
+				<div style={{paddingTop:"10px"}}>
 					<Routes>
-						<Route path='mainbar' element={<Mainbar />} />
+						<Route path='mainBar' element={<MainBar />} />
 						<Route path='about' element={<About />} />
 						<Route path='skills' element={<Skills />} />
 						<Route path='contact' element={<Contact />} />
 					</Routes>
 				</div>
 			</div>
-			</BrowserRouter>
+		</BrowserRouter>
 	);
 }
