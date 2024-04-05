d50ebf427e0408136d21dfa6b568cda8f42a9006
SHDarkStar <158495755+SHDarkStar@users.noreply.github.com>
2024-04-05 15:46:48 +0330

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,31 +1,35 @@
-import "./aboutStyle.css";
+import "./styles.css"; //وارد کردن فایل استایل ها, نوشتن بخش درباره ی من همراه با عکس 
 export default function About() {
 	return (
-		<section
-			className="aboutsec">
-			<div style={{"flex":"1 1 0%"}}>
-				<img src="http://localhost:5173/programming desktop.png"
-					style={{"width":"100%","height":"100%","backgroundSize":"cover"}}/>
-			</div>
-			<div className="aboutdiv">
+		<div>
+			<div className="aboutDiv">
 				<div>
-					<h2 style={{"fontSize":"3rem","lineHeight":1,"fontWeight":700,"textAlign":"center"}}>
-						About Me :
+					<h2 style={{"fontSize":"50px","textAlign":"center", "color":"black"}}>
+						<b>About Me :</b>
 					</h2>
 				</div>
-				<h3>
-					So hear is a paragraph about me and my interests.
-				</h3>
 				<p style={{"fontSize":"20px", "padding": "20px"}}>
-                I've been programming since starting college, and realize I'm not going to learn how to code at all!
-                    So as you expect, I am a passionate self-taught developer
-                    (which is quite a subjective thing as I learned from
-                    Quera and Maktabkhooneh.) I have experience coding with Python and want to start Django soon.
-                    I attended Fanapcampus classes and learned web designing from zero, now I'm a little familiar with React.
-                    Sadly, I've come to I couldn't find any interest in website design.
-                    Therefore I will continue to find the branch I'm looking for.  
+					Hello! My name is Setareh. I'm a programmer and web designer with a wide range of interests in the world of technology and coding.
+					I started my journey at university, where besides studying C++, and learning Python as a simple and widely-used language for practical purposes,
+					I've got a long way to go! <br />
+					As I became familiar with web development, I delved into the fundamentals of web design from scratch.
+					Currently, I'm eager to explore backend development and work with the Django framework.
+					<br />
+					Experiences:
+					<br />
+					1. Programming: <br />
+ 						I began with Python, which has been an excellent choice. Python is a powerful yet straightforward language for starters.
+						I'm familiar with OOP and some of its famous libraries. <br />
+					2. Web Design: <br />
+						I've grasped the principles of web design, including HTML, CSS, and JavaScript, to create dynamic and engaging websites. 
+					<br />
+					My plan is to resume learning, stay updated with new technologies, and develop meaningful, yet interesting projects.
+
 				</p>
 			</div>
-		</section>
+			<div className="aboutSection">
+				<img src="http://localhost:5173/gif2.gif" style={{"width":"100%","height":"100%","backgroundSize":"cover"}}/>
+			</div>
+		</div>
 	);
 }
