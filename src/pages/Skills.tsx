280c34b3aef62c836e77fd7dad52dbcd37629f87
SHDarkStar <158495755+SHDarkStar@users.noreply.github.com>
2024-04-05 15:49:08 +0330

    Update Skills.tsx

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,14 +1,13 @@
-import './skillsStyle.css';
+// همراه کردن فایل imageslider  با صفحه نایش دهنده skills و اعمال استایل به کل صفحه
 import ImageSlider from './ImageSlider';
-
-function Skills() {
+import "./styles.css";
+export default function Skills() {
   return (
-    <div>
+    <div  className='skills'>
     <h1>My Skills:</h1>
     <div>
       <ImageSlider />
     </div>
   </div>
   );
-};
-export default Skills;
+}
